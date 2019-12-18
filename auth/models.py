3413463aa4d80e15d0cdd2a76126646bdf3f9026
Frederick Johnson <fritzj96@gmail.com>
2019-12-18 10:22:41 -0800

    added: auth.User model in case I want to change it later

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
+from django.db.models import EmailField
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+  email = EmailField(max_length=100, unique=True)
+
+  USERNAME_FIELD = 'email'
