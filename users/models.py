06f4e6ccb0ee6baa8cab15c47b6b3c2509a453ff
Frederick Johnson <fritzj96@gmail.com>
2019-12-18 10:54:57 -0800

    added: is_superuser for permissions

--- users/models.py
+++ users/models.py
@@ -39,6 +39,7 @@ class UserManager(BaseUserManager):
         )
         user.is_staff = True
         user.is_admin = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
