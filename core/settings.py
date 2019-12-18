3f6d2df405e9de1de5a9f401a87d3e0af06e13a8
Frederick Johnson <fritzj96@gmail.com>
2019-12-18 09:55:16 -0800

    added: more whitenoise static file config

--- core/settings.py
+++ core/settings.py
@@ -126,4 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
