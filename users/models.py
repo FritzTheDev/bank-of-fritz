bfb6f6881c2f3ab8b9a7efe1d8633e34434b46f7
Frederick Johnson <fritzj96@gmail.com>
2019-12-18 10:29:46 -0800

    added: AbstractBaseUser thingy

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
+from django.db.models import EmailField
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+  email = EmailField(unique=True, max_length=100)
+
+  USERNAME_FIELD = 'email'
