34501d212ff7f85bd6eae512b94405b4ee1475a8
Frederick Johnson <fritzj96@gmail.com>
2019-12-18 09:59:51 -0800

    added: more staticfile config

--- core/settings.py
+++ core/settings.py
@@ -130,5 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')]
 
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
